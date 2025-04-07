82bdeef5d61a4910fb0a1a8592551259e1438f15
Neerajgowda2611 <neerajgowda2611@gmail.com>
2025-04-07 18:37:38 +0530

    removed unwanted folder and files

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,25 +1,3 @@
-// // src/lib/api.ts
-// import axios from 'axios'
-
-// const api = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL,
-// })
-
-// export const fetchResumes = async (userId: string) => {
-//   const response = await api.get(`/resumes?userId=${userId}`)
-//   return response.data
-// }
-
-// export const createResume = async (resumeData: Partial<Resume>) => {
-//   const response = await api.post('/resumes', resumeData)
-//   return response.data
-// }
-
-// export const updateResume = async (id: string, resumeData: Partial<Resume>) => {
-//   const response = await api.put(`/resumes/${id}`, resumeData)
-//   return response.data
-// }
-
 // lib/api.ts
 import axios from "axios";
 
